247c1fc066f3a34df20f89d5eab1ec7bffc9f29f
Marina Dargitz <marina.dargitz@gmail.com>
2018-08-09 11:17:28 -0600

    Add conditional config for production variables for easier deployment

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL = 
+process.env.NODE_ENV === 'production'
+  ? 'https://chronicle-server.herokuapp.com'
+  : 'http://localhost:8080';
